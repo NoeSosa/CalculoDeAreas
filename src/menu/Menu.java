8f2f6482e39113ca2d057d30f3fbfcf3a94a24ee
SP314-51 <SP314-51@192.168.1.215>
2020-03-01 19:18:58 -0600

    Repositorio de Calculo de areas

---
+++ src/menu/Menu.java
@@ -0,0 +1,19 @@
+
+package menu;
+
+
+public class Menu extends GUI {
+       
+        
+    public static void main(String[] args) {
+        
+        GUI menu = new GUI();
+        menu.setVisible(true);
+        
+
+        
+        
+    }
+    
+}
+
